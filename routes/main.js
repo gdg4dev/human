58f3c5668eedd22cdfe2aa1eb1844c614092a8d8
Digvijay Singh Rathore <dynamcidigvijay@gmail.com>
2020-05-06 21:49:53 +0530

    Human Backend

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const science = require('../controllers/science');
-
-router.post("/science", science);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-
-const mainRoutes = require("./routes/main");
+const fetch = require("node-fetch");
 
 app.use(bodyParser.json());
-app.use("/backend", mainRoutes)
+
+app.post("/backend/technology", (req, res) => {
+  fetch('http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=1df02f86f8594f81bf88d129c3f2094f')
+  .then(res => res.json())
+  .then((json) => {
+    const url = json.articles[0].url
+    fetch(`https://api.meaningcloud.com/summarization-1.0?key=f4be78fe9ca8f65345144ffc9305bce2&url=${url}&sentences=5`, { method: 'POST'})
+    .then(res => res.json())
+    .then((summary) => {
+      console.log("NEWS GENERATION SUCCESSFUL")
+      return res.send(summary)
+    })
+  })
+});
+
+app.post("/backend/science", (req, res) => {
+  fetch('http://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=1df02f86f8594f81bf88d129c3f2094f')
+  .then(res => res.json())
+  .then((json) => {
+    const url = json.articles[0].url
+    fetch(`https://api.meaningcloud.com/summarization-1.0?key=f4be78fe9ca8f65345144ffc9305bce2&url=${url}&sentences=5`, { method: 'POST'})
+    .then(res => res.json())
+    .then((summary) => {
+      console.log("NEWS GENERATION SUCCESSFUL")
+      return res.send(summary)
+    })
+  })
+});
+
+app.post("/backend/business", (req, res) => {
+  fetch('http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=1df02f86f8594f81bf88d129c3f2094f')
+  .then(res => res.json())
+  .then((json) => {
+    const url = json.articles[0].url
+    fetch(`https://api.meaningcloud.com/summarization-1.0?key=f4be78fe9ca8f65345144ffc9305bce2&url=${url}&sentences=5`, { method: 'POST'})
+    .then(res => res.json())
+    .then((summary) => {
+      console.log("NEWS GENERATION SUCCESSFUL")
+      return res.send(summary)
+    })
+  })
+});
 
 const port = 8000;
 app.listen(port, () => {
